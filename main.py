20193f087cc63e597559ba15bf34e1d0f48a25ac
ahamideh <ahamideh18@hotmail.com>
2023-02-16 20:21:47 -0500

    Add some stuff

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
+from cryptography.fernet import Fernet
+from argparse import ArgumentParser
+
 class Server:
-  def __init__(self):
-    print("Hello froms server")
+    def __init__(self):
+        print("Hello froms server")
+
+
+class Client:
+    def __init__(self):
+        print("Hello from client")
+
+
+if __name__ == '__main__':
+    roles = {'client': Client, 'server': Server}
+    parser = ArgumentParser()
+
+    parser.add_argument('-r', '--role',
+                        choices=roles,
+                        help='server or client role',
+                        required=True,
+                        type=str)
+
+    args = parser.parse_args()
+    roles[args.role]()
