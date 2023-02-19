f6b711fda08b60a91978d4a33a63c168ae9d9ca1
Faizan Rasool <faizanrasool@Faizans-MacBook-Pro.local>
2023-02-19 09:52:16 -0500

    server stays on after command is sent

--- main.py
+++ main.py
@@ -1,14 +1,43 @@
 from cryptography.fernet import Fernet
 from argparse import ArgumentParser
+import socket
 
 class Server:
     def __init__(self):
-        print("Hello froms server")
+        
+        HOST = 'localhost'
+        PORT = 10000
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((HOST, PORT))
+        s.listen()
+
+        conn, addr = s.accept()
+        print(f"Connected by {addr}")
+
+        with conn:
+            while True: 
+                while True:
+                    data = conn.recv(1024)
+                    if not data:
+                        conn, addr = s.accept()
+                        break
+                    conn.sendall(b"test")
 
 
+    
 class Client:
     def __init__(self):
-        print("Hello from client")
+
+        HOST = 'localhost'
+        PORT = 10000 
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((HOST, PORT))
+        s.sendall(b"Hello, world")
+        data = s.recv(1024)
+
+        print(f"Received {data!r}")
 
 
 if __name__ == '__main__':
